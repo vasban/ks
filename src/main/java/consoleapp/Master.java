290f005db61b82e62f5666cc6a4b1b6ba3e93c90
vasban <44297751+vasban@users.noreply.github.com>
2024-04-14 22:08:54 +0300

    Add files via upload

--- src/main/java/consoleapp/Master.java
+++ src/main/java/consoleapp/Master.java
@@ -1,10 +1,77 @@
-import java.util.ArrayList;
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
-public class Master{
+public class Master extends Thread {
 
-    ArrayList<Worker> workers = new ArrayList<Worker>();
+    private Socket socket = null;
+    private ObjectInputStream input = null;
+    private ObjectOutputStream output = null;
+    private String host;
+    private int port;
+    ArrayList<Accomodation> accArray ;
+
     
-    public static void main(String[] args){
-        
+    
+    public Master(String host,int port){
+
+        this.host = host;
+        this.port = port;
+
+
     }
+        
+    
+    public void run(){
+        try{
+            socket = new Socket(this.host,this.port);
+            Accomodation acc1 = new Accomodation("Bas",1,"Steri",5,0);
+            Accomodation acc2 = new Accomodation("Vas",1,"Peri",5,0);
+            output = new ObjectOutputStream(socket.getOutputStream());
+            input = new ObjectInputStream(socket.getInputStream());
+
+            output.writeObject(acc1);
+
+            
+            output.flush();
+
+            System.out.println("I am going to sleep!");
+
+            sleep(1000);
+
+            System.out.println("I got here!");
+
+
+        }catch (UnknownHostException e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }catch(IOException io){
+            System.out.println(io);
+        }catch(InterruptedException ie){
+            ie.printStackTrace();
+        }
+    
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+        // Worker w1 = new Worker(5000);
+        // Thread t1 = new Thread(w1);
+
+        // Worker w2 = new Worker(5001);
+        // Thread t2 = new Thread(w2);
+
+        // t1.start();
+        // t2.start();
+
+       Master m1 = new Master("localhost", 6969);
+
+       m1.start();
+       m1.port = 6968;
+       m1.start();
+
+
+    }
+
+
 }
--- src/main/java/consoleapp/Worker.java
+++ src/main/java/consoleapp/Worker.java
@@ -1,3 +1,84 @@
-public class Worker {
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+//Δες πως πρεπει να χωριστει το json 
+public class Worker extends Thread {
     
+    private Socket s = null;
+    private ServerSocket ss;
+    private ObjectInputStream input =null;
+    private ObjectOutputStream output =null;
+
+    ArrayList<Accomodation> accArray ;
+
+    @Override
+    public void run(){
+        System.out.println("Thread started");
+        System.out.println("Id " + Thread.currentThread().getName()
+        );
+        try{
+            
+
+            
+            
+            System.out.println("Client accepted");
+            Accomodation acc = (Accomodation) input.readObject();
+
+            acc.setRoomName("Bill was here");
+            System.out.println(acc.toString());
+            output.writeObject(acc);
+            output.flush();
+            
+
+
+        }catch(IOException io){
+            io.printStackTrace();
+        }catch(ClassNotFoundException c){
+            c.printStackTrace();
+        }
+
+    }
+
+    public Worker(int port){
+        try {
+            
+           this.ss = new ServerSocket(port);
+           System.out.println("Server Started");
+
+           System.out.println("Waiting for client");
+           this.s = ss.accept();
+           
+           this.output = new ObjectOutputStream(s.getOutputStream());
+			this.input = new ObjectInputStream(s.getInputStream());
+            System.out.println("I got here worker");
+
+          
+
+           //s = ss.accept();
+           
+
+        } catch (IOException e) {
+            // TODO: handle exception
+            System.out.println(e);
+
+        }
+    }
+
+    public static void main(String [] args){
+
+
+        Worker w1 = new Worker(6969);
+        Thread t1 = new Thread(w1);
+
+        Worker w2 = new Worker(6968);
+        Thread t2 = new Thread(w2);
+
+        
+        // Worker w2 = new Worker(5001);
+        // Thread t2 = new Thread(w2);
+
+        t1.start();
+        // t2.start();
+    }
 }
